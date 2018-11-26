0a50d4c7c181c886bdb0fe34c253cd2d234915c9
msavdert <melihsavdert@gmail.com>
2018-11-27 02:31:19 +0300

    Update ac.sql

--- ac.sql
+++ ac.sql
@@ -1,22 +1,3 @@
-set linesize 300
-set pagesize 9999
-set feedback on
-set longchunksize 999999
-set long 999999
-set trimout on
-set tab off
-
-col machine for a35
-col program for a40
-col event for a40
-col username for a20
-col Kill_Script for a60
-col wait_class for a15
-col sql_id for a15
-col inst_id for 99999
-col sid for 99999
-col SQL_HASH_VALUE for 9999999999
-
 SELECT inst_id,
   sid,
   serial#,
