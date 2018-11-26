a3696c07f70aa59facc6243c6262ce26c57095f8
msavdert <melihsavdert@gmail.com>
2018-11-27 02:18:04 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 SET sqlprompt "_user'@'_connect_identifier >"
 SET sqlformat ansiconsole
-define r=https://raw.githubusercontent.com/msavdert/sql/master/
+define r=https://raw.githubusercontent.com/msavdert/sql/master
 @https://raw.githubusercontent.com/msavdert/sql/master/i.sql
