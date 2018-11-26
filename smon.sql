f3af48064a0956ea9ebcdf58d782f8d47c91e4cc
msavdert <melihsavdert@gmail.com>
2018-11-27 02:29:03 +0300

    Update smon.sql

--- smon.sql
+++ smon.sql
@@ -1,3 +1,20 @@
+set linesize 300
+set pagesize 9999
+set feedback on
+set longchunksize 999999
+set long 999999
+set trimout on
+set space 1
+set tab off
+
+col sql_exec_start format a25
+col username format a25
+col status format a20
+col sess_serial format 99999
+col Kill_Script format a55
+col SQL_OPNAME format a10
+col sid format 999999
+
 SELECT *
      FROM
        (SELECT 
@@ -12,8 +29,8 @@ SELECT *
          TO_CHAR(sql_exec_start,'dd-mm-yyyy hh24:mi:ss')  AS sql_exec_start,
          CASE 
             WHEN (STATUS = 'EXECUTING')
-                THEN (sysdate-sql_exec_start) * 3600*24
-            ELSE (last_refresh_time-sql_exec_start) * 3600*24
+                THEN ROUND((sysdate-sql_exec_start) * 3600*24,0)
+            ELSE ROUND((last_refresh_time-sql_exec_start) * 3600*24,0)
          END AS "Elapsed (s)",
 --       ROUND(elapsed_time/1000000)                      AS "Elapsed (s)",
          ROUND(cpu_time    /1000000)                      AS "CPU (s)",
@@ -46,8 +63,8 @@ SELECT *
          TO_CHAR(sql_exec_start,'dd-mm-yyyy hh24:mi:ss')  AS sql_exec_start,
          CASE 
             WHEN (STATUS = 'EXECUTING')
-                THEN (sysdate-sql_exec_start) * 3600*24
-            ELSE (last_refresh_time-sql_exec_start) * 3600*24
+                THEN ROUND((sysdate-sql_exec_start) * 3600*24,0)
+            ELSE ROUN((last_refresh_time-sql_exec_start) * 3600*24,0)
          END AS "Elapsed (s)",
 --       ROUND(elapsed_time/1000000)                      AS "Elapsed (s)",
          ROUND(cpu_time    /1000000)                      AS "CPU (s)",
@@ -66,3 +83,11 @@ SELECT *
        )
      WHERE STATUS <> 'EXECUTING'
 	 AND rownum < 15;
+	 
+	-- sysdate-sql_exec_start ///execute ise değilse;
+	-- last refresh-sql_exec_start
+	 
+	-- case status='EXECUTING'
+	-- (sysdate-sql_exec_start) * 3600*24
+	--else 
+	-- (last_refresh_time-sql_exec_start* 3600*24
