8897bdb2e5c490b53eb06ec686ae6c176a03d0a6
msavdert <melihsavdert@gmail.com>
2018-11-27 02:25:18 +0300

    Create smon.sql

---
+++ smon.sql
@@ -0,0 +1,68 @@
+SELECT *
+     FROM
+       (SELECT 
+                     inst_id,
+                               sid,
+                               session_serial# sess_serial,
+                               status,
+         username,
+		 substr(sql_text,1,7) SQL_OPNAME,
+         sql_id,
+         sql_exec_id,
+         TO_CHAR(sql_exec_start,'dd-mm-yyyy hh24:mi:ss')  AS sql_exec_start,
+         CASE 
+            WHEN (STATUS = 'EXECUTING')
+                THEN (sysdate-sql_exec_start) * 3600*24
+            ELSE (last_refresh_time-sql_exec_start) * 3600*24
+         END AS "Elapsed (s)",
+--       ROUND(elapsed_time/1000000)                      AS "Elapsed (s)",
+         ROUND(cpu_time    /1000000)                      AS "CPU (s)",
+         buffer_gets,
+         ROUND(physical_read_bytes /(1024*1024)) AS "Phys reads (MB)",
+         ROUND(physical_write_bytes/(1024*1024)) AS "Phys writes (MB)",
+                               'ALTER SYSTEM KILL SESSION '''
+                               ||sid
+                               ||','
+                               ||session_serial#
+                               ||',@'
+                               ||inst_id
+                               || ''' IMMEDIATE;' Kill_Script
+       FROM gv$sql_monitor
+       ORDER BY  sql_exec_start DESC
+       )
+     WHERE STATUS = 'EXECUTING'
+	 UNION ALL
+	 SELECT *
+     FROM
+       (SELECT 
+                     inst_id,
+                               sid,
+                               session_serial# sess_serial,
+                               status,
+         username,
+		 substr(sql_text,1,7) SQL_OPNAME,
+         sql_id,
+         sql_exec_id,
+         TO_CHAR(sql_exec_start,'dd-mm-yyyy hh24:mi:ss')  AS sql_exec_start,
+         CASE 
+            WHEN (STATUS = 'EXECUTING')
+                THEN (sysdate-sql_exec_start) * 3600*24
+            ELSE (last_refresh_time-sql_exec_start) * 3600*24
+         END AS "Elapsed (s)",
+--       ROUND(elapsed_time/1000000)                      AS "Elapsed (s)",
+         ROUND(cpu_time    /1000000)                      AS "CPU (s)",
+         buffer_gets,
+         ROUND(physical_read_bytes /(1024*1024)) AS "Phys reads (MB)",
+         ROUND(physical_write_bytes/(1024*1024)) AS "Phys writes (MB)",
+                               'ALTER SYSTEM KILL SESSION '''
+                               ||sid
+                               ||','
+                               ||session_serial#
+                               ||',@'
+                               ||inst_id
+                               || ''' IMMEDIATE;' Kill_Script
+       FROM gv$sql_monitor
+       ORDER BY  sql_exec_start DESC
+       )
+     WHERE STATUS <> 'EXECUTING'
+	 AND rownum < 15;
