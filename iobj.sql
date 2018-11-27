c9473fe448a419c33525e05e508a386dfe8d1d2e
msavdert <melihsavdert@gmail.com>
2018-11-27 21:12:32 +0300

    Update iobj.sql

--- iobj.sql
+++ iobj.sql
@@ -1,6 +1,6 @@
 SELECT object_type, COUNT (object_type)
     FROM dba_objects
    WHERE     status <> 'VALID'
-         AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER (':1'))
+         AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('::1'))
 GROUP BY object_type
 ORDER BY object_type;
