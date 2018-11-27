9cd7d6f060d6394a8d3ff76078775d315cdde642
msavdert <melihsavdert@gmail.com>
2018-11-27 21:02:20 +0300

    Update iobj.sql

--- iobj.sql
+++ iobj.sql
@@ -1,55 +1,6 @@
 SELECT object_type, COUNT (object_type)
     FROM dba_objects
    WHERE     status <> 'VALID'
-         AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('||:one||'))
+         AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER (:1 ))
 GROUP BY object_type
 ORDER BY object_type;
-/
-
-SELECT    'create or replace synonym '
-       || owner
-       || '.'
-       || object_name
-       || ' for '
-       || 'sch_dmall'
-       || '.'
-       || object_name
-       || ';'
-           AS cmd
-  FROM dba_objects
- WHERE     object_type = 'SYNONYM'
-       AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('||:one||'))
-       AND status = 'INVALID';
-       /
-
-SELECT    'alter '
-       || object_type
-       || ' '
-       || owner
-       || '.'
-       || object_name
-       || ' compile;'
-           AS script
-  FROM dba_objects
- WHERE     object_type IN ('PROCEDURE',
-                           'MATERIALIZED VIEW',
-                           'VIEW',
-                           'FUNCTION',
-                           'TRIGGER',
-                           'TYPE',
-                           'PACKAGE')
-       AND status = 'INVALID'
-       AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('||:one||'));
-       /
-
-SELECT    'alter package '
-       || owner
-       || '.'
-       || object_name
-       || ' compile body;'
-           AS script
-  FROM dba_objects
- WHERE     object_type IN ('TYPE BODY', 'PACKAGE BODY')
-       AND status = 'INVALID'
-       AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('||:one||'));
-/
