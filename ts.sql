b98267d9cfbda39a682bd246510abc7c6829c554
msavdert <melihsavdert@gmail.com>
2019-08-15 02:12:15 +0300

    Update ts.sql

--- ts.sql
+++ ts.sql
@@ -1,6 +1,6 @@
 select tablespace_name,tablespace_size,used_space,
     case 
-        when used_percent > 95 then '@|bg_red '||round(used_percent)||'|@'
+        when used_percent > 95 then '@|red '||round(used_percent)||'|@'
         else ''||round(used_percent)||''
     end as ts_usage_percentage
 from dba_tablespace_usage_metrics order by 4 desc;
