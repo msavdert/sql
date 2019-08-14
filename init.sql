ece53538d6a7a298f33b36ac2f21c124d6a7f1f1
msavdert <melihsavdert@gmail.com>
2019-08-14 23:47:28 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -4,13 +4,14 @@ SET sqlformat ansiconsole
 SET FEEDBACK OFF
 --old value > new value
 SET VERIFY OFF
+SET LINES 999 PAGES 999
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS';
 ALTER SESSION SET NLS_TIMESTAMP_FORMAT='YYYY-MM-DD HH24:MI:SS.FF';
 ALTER SESSION SET NLS_LANGUAGE='AMERICAN';
 
 alias load https://raw.githubusercontent.com/msavdert/sql/master/aliases.xml
 
---@https://raw.githubusercontent.com/msavdert/sql/master/i.sql
+@https://raw.githubusercontent.com/msavdert/sql/master/whoami.sql
 
 --Define
 --define r=https://raw.githubusercontent.com/msavdert/sql/master
