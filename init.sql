c7ab98d3f6b40ca46b5e41bed0a6812b2389f4e6
msavdert <melihsavdert@gmail.com>
2019-08-14 23:31:22 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -7,12 +7,15 @@ SET VERIFY OFF
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS';
 ALTER SESSION SET NLS_TIMESTAMP_FORMAT='YYYY-MM-DD HH24:MI:SS.FF';
 ALTER SESSION SET NLS_LANGUAGE='AMERICAN';
---@https://raw.githubusercontent.com/msavdert/sql/master/i.sql
---Define
-define r=https://raw.githubusercontent.com/msavdert/sql/master
 
+alias load https://raw.githubusercontent.com/msavdert/sql/master/aliases.xml
+
+--@https://raw.githubusercontent.com/msavdert/sql/master/i.sql
+
+--Define
+--define r=https://raw.githubusercontent.com/msavdert/sql/master
 -- aliases.xml
-alias ac=@&r\ac;
-alias desc ac Active Sessions
-alias backup=@&r\backup :DAY;
-alias desc backup backup <DAY> - show last N DAY Backup 
+--alias ac=@&r\ac;
+--alias desc ac Active Sessions
+--alias backup=@&r\backup :DAY;
+--alias desc backup backup <DAY> - show last N DAY Backup 
