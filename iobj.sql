8dd04e96503241b4dc09c0d1af296587deceea7f
msavdert <melihsavdert@gmail.com>
2019-08-15 11:38:17 +0300

    Update iobj.sql

--- iobj.sql
+++ iobj.sql
@@ -1,52 +1,10 @@
-SELECT object_type, COUNT (object_type)
+SELECT owner owner,
+       object_type,
+       object_name,
+       status,
+       TO_CHAR (created, 'YYYY-MM-DD HH24:MI:SS') created,
+       TO_CHAR (last_ddl_time, 'YYYY-MM-DD HH24:MI:SS') last_ddl_time,
+       TO_CHAR (TO_DATE (timestamp, 'YYYY-MM-DD HH24:MI:SS'), 'YYYY-MM-DD HH24:MI:SS') timestamp
     FROM dba_objects
-   WHERE     status <> 'VALID'
-         AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('&1'))
-GROUP BY object_type
-ORDER BY object_type;
-
-SELECT    'create or replace synonym '
-       || owner
-       || '.'
-       || object_name
-       || ' for '
-       || 'sch_dmall'
-       || '.'
-       || object_name
-       || ';'
-           AS cmd
-  FROM dba_objects
- WHERE     object_type = 'SYNONYM'
-       AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('&&1'))
-       AND status = 'INVALID';
-
-SELECT    'alter '
-       || object_type
-       || ' '
-       || owner
-       || '.'
-       || object_name
-       || ' compile;'
-           AS script
-  FROM dba_objects
- WHERE     object_type IN ('PROCEDURE',
-                           'MATERIALIZED VIEW',
-                           'VIEW',
-                           'FUNCTION',
-                           'TRIGGER',
-                           'TYPE',
-                           'PACKAGE')
-       AND status = 'INVALID'
-       AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('&&1'));
-       
-
-SELECT    'alter package '
-       || owner
-       || '.'
-       || object_name
-       || ' compile body;'
-           AS script
-  FROM dba_objects
- WHERE     object_type IN ('TYPE BODY', 'PACKAGE BODY')
-       AND status = 'INVALID'
-       AND (owner LIKE 'USR%' OR LOWER (owner) = LOWER ('&&1'));
+   WHERE status != 'VALID' --AND LAST_DDL_TIME >= SYSDATE - 1
+ORDER BY last_ddl_time DESC;
