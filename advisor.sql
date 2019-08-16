e26016523dd2d9f953057d5470bf9b60dca08fe0
msavdert <melihsavdert@gmail.com>
2019-08-16 19:03:55 +0300

    Update advisor.sql

--- advisor.sql
+++ advisor.sql
@@ -9,9 +9,9 @@ DBMS_OUTPUT.put_line('You can execute task using below syntax');
 DBMS_OUTPUT.put_line('exec DBMS_SQLTUNE.EXECUTE_TUNING_TASK(task_name => '''||stmt_task||''');');
 dbms_output.new_line();
 DBMS_OUTPUT.put_line('You can monitor task status using below syntax');
-DBMS_OUTPUT.put_line('SELECT task_name, status, pct_completion_time FROM dba_advisor_log WHERE task_name = '''||stmt_task||''');');
+DBMS_OUTPUT.put_line('SELECT task_name, status, pct_completion_time FROM dba_advisor_log WHERE task_name = '''||stmt_task||''';');
 dbms_output.new_line();
 DBMS_OUTPUT.put_line('You can check advisor recommendations using below syntax');
-DBMS_OUTPUT.put_line('SELECT DBMS_SQLTUNE.report_tuning_task( '''||stmt_task||''') AS recommendations FROM dual;');
+DBMS_OUTPUT.put_line('SELECT DBMS_SQLTUNE.report_tuning_task('''||stmt_task||''') AS recommendations FROM dual;');
 end;
 /
