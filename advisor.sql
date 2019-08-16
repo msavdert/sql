5e4751c06b3fb4312debb2f951467ee670b04df2
msavdert <melihsavdert@gmail.com>
2019-08-16 19:09:36 +0300

    Update advisor.sql

--- advisor.sql
+++ advisor.sql
@@ -6,7 +6,7 @@ stmt_task := DBMS_SQLTUNE.CREATE_TUNING_TASK(sql_id => :sql_id, time_limit => 36
 DBMS_OUTPUT.put_line('task_id: ' || stmt_task );
 dbms_output.new_line();
 DBMS_OUTPUT.put_line('You can execute task using below syntax');
-'@|blue DBMS_OUTPUT.put_line('exec DBMS_SQLTUNE.EXECUTE_TUNING_TASK(task_name => '''||stmt_task||''');');|@'
+DBMS_OUTPUT.put_line('exec DBMS_SQLTUNE.EXECUTE_TUNING_TASK(task_name => '''||stmt_task||''');');
 dbms_output.new_line();
 DBMS_OUTPUT.put_line('You can monitor task status using below syntax');
 DBMS_OUTPUT.put_line('SELECT task_name, status, pct_completion_time FROM dba_advisor_log WHERE task_name = '''||stmt_task||''';');
