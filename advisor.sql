9f43e5cc3ec7250aae6dfc5bf71597c720b1e9f0
msavdert <melihsavdert@gmail.com>
2019-08-16 18:55:12 +0300

    Update advisor.sql

--- advisor.sql
+++ advisor.sql
@@ -2,7 +2,7 @@ SET SERVEROUTPUT ON
 declare
 stmt_task VARCHAR2(40);
 begin
-stmt_task := DBMS_SQLTUNE.CREATE_TUNING_TASK(sql_id => :sql_id, time_limit => 3600);
+stmt_task := DBMS_SQLTUNE.CREATE_TUNING_TASK(sql_id => '&sql_id', time_limit => 3600);
 DBMS_OUTPUT.put_line('task_id: ' || stmt_task );
 dbms_output.new_line();
 DBMS_OUTPUT.put_line('You can execute task using below syntax');
@@ -10,5 +10,8 @@ DBMS_OUTPUT.put_line('begin DBMS_SQLTUNE.EXECUTE_TUNING_TASK(task_name => '''||s
 dbms_output.new_line();
 DBMS_OUTPUT.put_line('You can monitor task status using below syntax');
 DBMS_OUTPUT.put_line('SELECT task_name, status, pct_completion_time FROM dba_advisor_log WHERE task_name = '''||stmt_task||''');');
+dbms_output.new_line();
+DBMS_OUTPUT.put_line('You can check advisor recommendations using below syntax');
+DBMS_OUTPUT.put_line('SELECT DBMS_SQLTUNE.report_tuning_task( '''||stmt_task||''') AS recommendations FROM dual;');
 end;
 /
