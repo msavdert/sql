a4b5803fb452cfe9ca18c65edb48c3f94d72b142
msavdert <melihsavdert@gmail.com>
2019-08-16 21:05:55 +0300

    Update xpi.sql

--- xpi.sql
+++ xpi.sql
@@ -1,5 +1,5 @@
 -- Copyright 2018 Tanel Poder. All rights reserved. More info at http://tanelpoder.com
 -- Licensed under the Apache License, Version 2.0. See LICENSE.txt for terms & conditions.
 
-prompt eXplain with Profile: Running DBMS_SQLTUNE.REPORT_SQL_MONITOR for SQL_ID &1....
-@@https://raw.githubusercontent.com/msavdert/sql/master/xprof.sql ALL TEXT SQL_ID "'&1'"
+prompt eXplain with Profile: Running DBMS_SQLTUNE.REPORT_SQL_MONITOR for SQL_ID :1....
+@@https://raw.githubusercontent.com/msavdert/sql/master/xprof.sql ALL TEXT SQL_ID "':1'"
