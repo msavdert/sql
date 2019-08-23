2a843256e8c5ec5e279a5ffa998d805c0b9c1793
msavdert <melihsavdert@gmail.com>
2019-08-23 16:15:44 +0300

    Update smon.sql

--- smon.sql
+++ smon.sql
@@ -1,3 +1,5 @@
+DEF v_minute = '&1'
+
 WITH sql_monitor_stats AS (
     SELECT
         m.sql_id,
@@ -206,6 +208,6 @@ SELECT /*+NO_MONITOR*/
 FROM
     sql_monitor          m,
     sql_monitor_limits   l
-WHERE m.sql_exec_start > (SYSTIMESTAMP - INTERVAL '&1' MINUTE) 
+WHERE m.sql_exec_start > (SYSTIMESTAMP - INTERVAL '&v_minute' MINUTE) 
 -- m.sql_exec_start > sysdate-1/48
 ORDER BY m.sql_exec_start DESC;
