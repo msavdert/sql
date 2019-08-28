db5059aa0b92d202b674e239d73a2938bb24104e
msavdert <melihsavdert@gmail.com>
2019-08-28 11:48:37 +0300

    Update taila.sql

--- taila.sql
+++ taila.sql
@@ -2,5 +2,5 @@ WITH oneday AS (
 SELECT /*+ materialize */ * FROM TABLE(gv$(cursor(select inst_id, originating_timestamp, message_text FROM v$diag_alert_ext 
 WHERE ORIGINATING_TIMESTAMP > SYSTIMESTAMP - 1 AND UPPER(filename) LIKE (select '%'||UPPER(name)||'%' from v$database)))))
 SELECT INST_ID c1, TO_CHAR (ORIGINATING_TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS') c2, MESSAGE_TEXT c3 FROM oneday
- WHERE originating_timestamp > (SYSTIMESTAMP - INTERVAL ':1' MINUTE)
+ WHERE originating_timestamp > sysdate-:1/1440
 ORDER BY c2 DESC;
