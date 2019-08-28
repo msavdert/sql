c7a6b985d79b1f3c416dbedd513cc3d61b413e88
msavdert <melihsavdert@gmail.com>
2019-08-28 15:01:08 +0300

    Update err.sql

--- err.sql
+++ err.sql
@@ -1,1 +1,21 @@
-select * from SYS.INS_ERROR_SUMMARY where error_date > sysdate-:1/1440 order by error_date desc;
+SELECT
+    username,
+    error_number,
+    substr(error_text, 1,30) error_text,
+    instance_number inst_id,
+    substr(sql_text, 1,6) sql_text,
+    session_user,
+    current_user,
+    current_schema,
+    host,
+    ip_address,
+    os_user,
+    terminal,
+    service_name,
+    to_char(error_date, 'DD/MON/YY HH24:MI:SS') error_date
+FROM
+    sys.ins_error_summary
+WHERE
+    error_date > sysdate - :1 / 1440
+ORDER BY
+    error_date DESC;
