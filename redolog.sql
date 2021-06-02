07ef198fef353aa1805467480c02903ffa0e5c05
msavdert <melihsavdert@gmail.com>
2021-06-02 19:45:05 +0300

    Update redolog.sql

--- redolog.sql
+++ redolog.sql
@@ -12,3 +12,15 @@ SELECT l.thread#,
 FROM   v$logfile lf
        JOIN v$log l ON l.group# = lf.group#
 ORDER BY l.thread#,lf.group#, lf.member;
+
+SELECT sl.thread#,
+         sl.group#,
+         l.MEMBER,
+         TRUNC (sl.bytes / 1024 / 1024) AS size_mb,
+         sl.status,
+         sl.archived,
+         l.TYPE,
+         l.is_recovery_dest_file AS rdf,
+         sl.sequence#
+    FROM v$standby_log sl JOIN v$logfile l ON l.group# = sl.group#
+ORDER BY thread#, group#;
